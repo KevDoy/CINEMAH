7bd4fccbdd4a9f1ab1399e3c289bdb87cbbcf59f
KevDoy <KevDoy@users.noreply.github.com>
2023-02-24 12:38:29 -0800

    Version 1.0
    Move to JSON based generation
    New layout
    Prefilled content

--- js/login.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-function process()
-{
-var url=window.location.protocol + "//" + document.getElementById("user").value + ":" + document.getElementById("pass").value + "@" + window.location.host + "/start/index.html?=" + document.getElementById("user").value;
-location.href=url;
-return false;
-}
